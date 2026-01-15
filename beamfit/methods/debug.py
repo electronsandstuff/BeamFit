d0e963cc54d62dd3766bf3ef113e33f707cadd8d
Christopher M. Pierce <contact@chris-pierce.com>
2026-01-14 17:13:03 -0800

    adding docstrings

--- beamfit/methods/debug.py
+++ beamfit/methods/debug.py
@@ -2,6 +2,10 @@ from ..base import AnalysisMethod
 
 
 class AnalysisMethodDebugger(AnalysisMethod):
+    """
+    Analysis method with `fit` that just passes the image out of it to test image pre-processing functions.
+    """
+
     def __init__(self, **kwargs):
         super(AnalysisMethodDebugger, self).__init__(**kwargs)
 
