bfa87e97ea12cbfb9b417ed5dd9cd230d2b7bf35
Christopher M. Pierce <contact@chris-pierce.com>
2026-01-14 17:42:44 -0800

    docstrings

--- beamfit/factory.py
+++ beamfit/factory.py
@@ -2,7 +2,18 @@ registered_objects = {}
 
 
 def register(objtype: str, name: str, createfun: callable):
-    """Registers the creation function with the given name and an object type"""
+    """
+    Registers the creation function with the given name and an object type
+
+    Parameters
+    ----------
+    objtype : str
+        The type of the object to register
+    name : str
+        The name of the object to register
+    createfun : function
+        The constructor of the object
+    """
     if objtype not in registered_objects:
         registered_objects[objtype] = {}
     if name in registered_objects[objtype]:
@@ -11,13 +22,44 @@ def register(objtype: str, name: str, createfun: callable):
 
 
 def create(objtype: str, name: str, **kwargs):
-    """Returns an object created with the provided keyword arguments"""
+    """
+    Returns an object created with the previously registered constructor. Any additional arguments
+    are passed to the constructor as keyword arguments.
+
+    objtype : str
+        The type of object to create
+    name : str
+        The name of the object to create
+    """
     return registered_objects[objtype][name](**kwargs)
 
 
 def unregister(objtype: str, name: str):
+    """
+    Remove an object from the registry.
+
+    Parameters
+    ----------
+    objtype : str
+        The type of the object to unregister
+    name : str
+        The name of the object to unregister
+    """
     registered_objects[objtype].pop(name)
 
 
-def get_names(objtype: str):
+def get_names(objtype: str) -> list[str]:
+    """
+    Return a list of names registered to this object type
+
+    Parameters
+    ----------
+    objtype : str
+        The type of the object to look up
+
+    Returns
+    -------
+    list[str]
+        The names of the registered objects
+    """
     return list(registered_objects[objtype].keys())
